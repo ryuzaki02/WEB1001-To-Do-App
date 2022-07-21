eaae546ba14ee4257a5c207ac2c2a0d35aa3f15e
Aman <thakur.aman77@yahoo.com>
2022-07-21 17:08:59 -0400

    First Commit

---
+++ WEB1001-To-Do-App/Models/ErrorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WEB1001_To_Do_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
+
