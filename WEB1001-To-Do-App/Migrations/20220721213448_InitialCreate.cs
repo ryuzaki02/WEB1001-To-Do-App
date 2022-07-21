86176015a43ac714adcbe2aaa2356ea59056c2ce
Aman <thakur.aman77@yahoo.com>
2022-07-21 17:36:56 -0400

    - Added database frameworks - Generated database files - Removed namespace

---
+++ WEB1001-To-Do-App/Migrations/20220721213448_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WEB1001_To_Do_App.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ToDo",
+                columns: table => new
+                {
+                    ToDoModelId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    IsCompleted = table.Column<bool>(type: "INTEGER", nullable: false),
+                    CompletionDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ToDo", x => x.ToDoModelId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ToDo");
+        }
+    }
+}
--- WEB1001-To-Do-App/Models/ToDoModel.cs
+++ WEB1001-To-Do-App/Models/ToDoModel.cs
@@ -1,9 +1,37 @@
 ﻿using System;
-namespace WEB1001_To_Do_App.Models
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace WEB1001_To_Do_App
 {
-	public class ToDoModel
+    public class ToDoContext : DbContext
+    {
+        public DbSet<ToDoModel> ToDo => Set<ToDoModel>();
+
+        public string DbPath { get; }
+
+        public ToDoContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Join(path, "toDo.db");
+        }
+
+        // The following configures EF to create a Sqlite database file in the
+        // special "local" folder for your platform.
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite($"Data Source={DbPath}");
+    }
+
+    public class ToDoModel
 	{
-		
+		public int ToDoModelId { get; set; }
+		[Required]
+		public bool IsCompleted { get; set; }
+		[Required]
+		public DateTime CompletionDate { get; set; }
+		[Required]
+		public string Description { get; set; }
 	}
 }
-
